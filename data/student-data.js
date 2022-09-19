b015249d443e9bbae38a1442af44d296ac430f91
Carol Kang <otherhats@gmail.com>
2022-09-19 17:50:14 -0400

    initial commit

---
+++ data/student-data.js
@@ -0,0 +1,9 @@
+const students = [
+  {firstName: 'Carol', lastName: 'Kang', graduated: false; _studentId: 12345},
+  {firstName: 'Jane', lastName: 'Doe', graduated: true; _studentId: 12346},
+  {firstName: 'John', lastName: 'Smith', graduated: false; _studentId: 12347},
+]
+
+export {
+	students
+}
