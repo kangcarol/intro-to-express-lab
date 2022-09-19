55f4660343178cbd245409f5c42df77b64237adb
Carol Kang <otherhats@gmail.com>
2022-09-19 19:03:25 -0400

    complete foundation of the lab, explore bootstrap

--- server.js
+++ server.js
@@ -1,1 +1,34 @@
-git 
+// import modules
+import express from 'express'
+
+// import student data
+// import { students } from './data/students-data.js'
+import { students } from './data/student-data.js'
+
+// create Express app
+const app = express()
+
+// Configure the app (app.set)
+app.set('view engine', 'ejs')
+
+// Mount Middleware (app.use)
+
+// Mount routes
+app.get ('/home', function(req, res){
+  res.render ('home')
+})
+
+app.get('/', function(req, res) {
+  res.redirect('/home')
+})
+
+app.get('/students', function(req, res){
+  res.render('students/index', {
+    students: students
+  })
+})
+
+// Tell the app to listen on port 3000
+app.listen(3000, function() {
+  console.log('Listening on port 3000')
+})
